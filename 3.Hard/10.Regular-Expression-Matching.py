e1d8c8aac08fe35788f86de77052930b8984905b
Connection <69660530+ConnectionOuOb@users.noreply.github.com>
2024-03-07 15:36:48 +0800

    Update 10.Regular-Expression-Matching.py

--- 3.Hard/10.Regular-Expression-Matching.py
+++ 3.Hard/10.Regular-Expression-Matching.py
@@ -8,38 +8,38 @@ The matching should cover the entire input string (not partial).
 
 class Solution(object):
     def isMatch(self, s, p):
-        """
-        :type s: str
-        :type p: str
-        :rtype: bool
-        """
+        m, n = len(s), len(p)
+        dp = [[False] * (n+1) for _ in range(m+1)]
+        dp[0][0] = True
 
-        pointer = 0
-        pLen = len(p)
-        print("#####", s, p, pLen)
-        for char in s:
-            if pointer == pLen:
-                print("##### pointer == pLen")
-                return False
-            
-            if char == p[pointer] or p[pointer] == ".":
-                print("##", pointer, char, p[pointer])
-                pointer += 1
-            elif p[pointer] == "*":
-                if pointer+1 < pLen:
-                    print("##", pointer, pLen, char, p[pointer], p[pointer+1])
-                    pointer += 1
-            else:
-                print("# ", pointer, pLen, char, p[pointer]) 
-                return False
+        for j in range(1, n+1):
+            if p[j-1] == '*':
+                dp[0][j] = dp[0][j-2]
 
-        return True
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if s[i-1] == p[j-1] or p[j-1] == '.':
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1] == '*':
+                    dp[i][j] = dp[i][j-2]
+                    if s[i-1] == p[j-2] or p[j-2] == '.':
+                        dp[i][j] |= dp[i-1][j]
+
+        return dp[m][n]
 
 
 if __name__ == "__main__":
     s = Solution()
-#    print(s.isMatch("aa", "a"))
-#    print(s.isMatch("aa", "a*"))
-#    print(s.isMatch("ab", ".*"))
-#    print(s.isMatch("aab", "c*a*b"))
+    print(s.isMatch("aaca", "ab*a*c*a"))
+    exit()
+    print(s.isMatch("mississippi", "mis*is*ip*."))
+    exit()
     print(s.isMatch("mississippi", "mis*is*p*."))
+    print(s.isMatch("bbab", "b*a*"))
+    print(s.isMatch("a", "ab*a"))  # False
+    print(s.isMatch("a", "ab*"))  # True
+    print(s.isMatch("aaca", "ab*a*c*a"))
+    print(s.isMatch("aa", "a*"))
+    print(s.isMatch("aa", "a"))
+    print(s.isMatch("ab", ".*"))
+    print(s.isMatch("aab", "c*a*b"))
