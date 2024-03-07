75576821fb19f5c9cfeda5e469b2d79295f96faf
Connection <69660530+ConnectionOuOb@users.noreply.github.com>
2024-03-07 15:52:30 +0800

    Update 10.Regular-Expression-Matching.py

--- 3.Hard/10.Regular-Expression-Matching.py
+++ 3.Hard/10.Regular-Expression-Matching.py
@@ -8,14 +8,17 @@ The matching should cover the entire input string (not partial).
 
 class Solution(object):
     def isMatch(self, s, p):
+        # Initialize the dp array
         m, n = len(s), len(p)
         dp = [[False] * (n+1) for _ in range(m+1)]
         dp[0][0] = True
 
+        # Set different entry
         for j in range(1, n+1):
             if p[j-1] == '*':
                 dp[0][j] = dp[0][j-2]
 
+        # Fill the dp array
         for i in range(1, m+1):
             for j in range(1, n+1):
                 if s[i-1] == p[j-1] or p[j-1] == '.':
@@ -31,9 +34,7 @@ class Solution(object):
 if __name__ == "__main__":
     s = Solution()
     print(s.isMatch("aaca", "ab*a*c*a"))
-    exit()
     print(s.isMatch("mississippi", "mis*is*ip*."))
-    exit()
     print(s.isMatch("mississippi", "mis*is*p*."))
     print(s.isMatch("bbab", "b*a*"))
     print(s.isMatch("a", "ab*a"))  # False
